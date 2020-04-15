73937ad9d6dbd868a0be1abd3224138cbcffe466
andrewnoelcasper <casperson2@me.com>
2020-04-14 22:38:40 -0700

    Code pushed rather than the executable

---
+++ origin/CoronavirusCounter.java
@@ -0,0 +1,44 @@
+// Authors: Andrew Casperson & Thomas Lasbury
+
+//Basic UI Libraries 
+import java.awt.*;
+import javax.swing.*;
+
+public class CoronavirusCounter
+{
+	
+public static void main(String []args)
+    { 
+        CoronavirusCounter c = new CoronavirusCounter();
+        CoronavirusCounter.createWindow();
+    }
+    
+    // instance variables - replace the example below with your own
+    
+    private static void createWindow()
+    {
+         JFrame frame = new JFrame("Coronavirus Counter");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.getContentPane().setBackground(Color.black);
+         frame.setSize(1000,800);
+         //This displays the wondow
+         frame.setLocationRelativeTo(null);
+
+         frame.setVisible(true);
+    }
+
+    /*
+     * Constructor for objects of class test
+     */
+    
+    public CoronavirusCounter()
+    {
+        // initialise instance variables
+
+    }
+
+    
+
+
+}
+
